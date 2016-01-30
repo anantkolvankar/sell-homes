abb88af5c50e198775a7f11a2277f7e5850e34eb
go-ruby <go.ruby405@gmail.com>
2016-01-30 23:37:25 +0530

    started property show page integration

--- app/models/specification.rb
+++ app/models/specification.rb
@@ -1,3 +1,6 @@
 class Specification < ActiveRecord::Base
   belongs_to :property
+  belongs_to :super_builtup_unit, class_name: 'Unit', foreign_key: :super_builtup_unit
+  belongs_to :builtup_unit, class_name: 'Unit', foreign_key: :builtup_unit
+  belongs_to :carpet_unit, class_name: 'Unit', foreign_key: :carpet_unit
 end
